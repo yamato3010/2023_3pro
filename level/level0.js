2efa353fa70621ae9f1bf45046c4043c3d98c913
Yamato <75778273+yamato3010@users.noreply.github.com>
2024-03-28 09:19:05 +0000

    盤面を動的に生成できるように

--- level/level0.js
+++ level/level0.js
@@ -14,13 +14,13 @@ mondai[0] = {
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ]
 };
-
+// loc は [x, y]
 mondai[0].quiz =
     [
-        { kagi: "あきらって誰？", ans: "あきら", loc: [0, 0], direction: "vertical" },
-        { kagi: "あいうえ", ans: "あいうえ", loc: [0, 0], direction: "horizontal" },
-        { kagi: "かきくけ", ans: "あいうえ", loc: [3, 1], direction: "horizontal" },
-        { kagi: "チョコレートうまい", ans: "あいうえ", loc: [3, 1], direction: "vertical" }
+        { kagi: "あきらって誰？", ans: "a ki ra", loc: [0, 0], direction: "vertical" },
+        { kagi: "あいうえ", ans: "a i u e", loc: [4, 0], direction: "horizontal" },
+        { kagi: "かきくけ", ans: "ka ki ku ke", loc: [1, 3], direction: "horizontal" },
+        { kagi: "チョコレートうまい", ans: "a i u", loc: [1, 7], direction: "vertical" }
     ];
 
 const board = document.querySelector("#board");
@@ -30,9 +30,11 @@ const board = document.querySelector("#board");
 <div class="cell-content" data-correct-letter="a" id="cell-1"></div>
 </div> */}
 
+let cellNum = 0;
 
 for (let cy = 0; cy < mondai[0].board.length; cy++) {
     for (let cx = 0; cx < mondai[0].board[cy].length; cx++) {
+        cellNum ++;
         console.log(mondai[0].board[cy][cx]);
         let cell_elm = document.createElement("div");
         cell_elm.className = "cell";
@@ -42,6 +44,9 @@ for (let cy = 0; cy < mondai[0].board.length; cy++) {
             span_elm.innerHTML = mondai[0].board[cy][cx];
         }
         let cell_con_elm = document.createElement("div");
+        // class名とセル番号を追加
+        cell_con_elm.className = "cell-content";
+        cell_con_elm.id = "cell-" + cellNum;
         cell_elm.appendChild(span_elm);
         cell_elm.appendChild(cell_con_elm);
         board.appendChild(cell_elm);
@@ -50,8 +55,10 @@ for (let cy = 0; cy < mondai[0].board.length; cy++) {
 
 const vertical_key = document.querySelector("#vertical");
 const horizontal_key = document.querySelector("#horizontal");
+let dataCheckGroups = []
 
 for (let q of mondai[0].quiz) {
+    // ヒントエリアの描画
     console.log(q);
     let q_elm = document.createElement("li");
     q_elm.innerHTML = mondai[0].board[q.loc[0]][q.loc[1]]+ "." + q.kagi;
@@ -60,5 +67,57 @@ for (let q of mondai[0].quiz) {
     } else if (q.direction == "horizontal") {
         horizontal_key.appendChild(q_elm);
     }
+
+    dataCheckGroups.push({
+      specificCells: [],
+      alertShown: false
+    })
+  
+    // cellのdata-correct-letterプロパティに正解の文字を追加
+    let ansArr = q.ans.split(" ");
+    // 縦の場合
+    if (q.direction == "vertical") {
+      let y = q.loc[1];
+        // 答えの配列でループ
+        for (let i = 0; i < ansArr.length; i++) {
+          if(y == 0) {
+            console.log("答えを追加：#cell-" + (q.loc[0] + 1) + ":" + ansArr[i])
+            let element = document.getElementById("cell-" + (q.loc[0] + 1));
+            element.setAttribute("data-correct-letter", ansArr[i]);
+            dataCheckGroups[dataCheckGroups.length - 1].specificCells.push("#cell-" + (q.loc[0] + 1));
+          } else {
+            console.log("答えを追加：#cell-" + y + (q.loc[0] + 1) + ":" + ansArr[i])
+            let element = document.getElementById("cell-" + y + (q.loc[0] + 1));
+            element.setAttribute("data-correct-letter", ansArr[i]);
+            dataCheckGroups[dataCheckGroups.length - 1].specificCells.push("#cell-" + y + (q.loc[0] + 1));
+          }
+          y ++;
+        }
+    // 横の場合  
+    } else if (q.direction == "horizontal") {
+        let x = q.loc[0];
+        let y = q.loc[1];
+        // 答えの配列でループ
+        for (let i = 0; i < ansArr.length; i++) {
+            if (y == 0){
+              console.log("答えを追加：#cell-" + (x + 1) + ":" + ansArr[i])
+              let element = document.getElementById("cell-" + (x + 1));
+              element.setAttribute("data-correct-letter", ansArr[i]);
+              dataCheckGroups[dataCheckGroups.length - 1].specificCells.push("#cell-" + (x + 1))
+            } else {
+              console.log("答えを追加：#cell-" + y + (x + 1) + ":" + ansArr[i])
+              let element = document.getElementById("cell-" + y + (x + 1));
+              element.setAttribute("data-correct-letter", ansArr[i]);
+              dataCheckGroups[dataCheckGroups.length - 1].specificCells.push("cell-" + y + (x + 1))
+            }
+          x ++;
+        }
+    }
+    // bodyタグのdata-check-groupsプロパティにJSON形式の文字列を追加
+    // bodyタグを取得
+    let body_elm = document.querySelector("body");
+    // data-check-groupsプロパティを追加
+    body_elm.setAttribute("data-check-groups", JSON.stringify(dataCheckGroups));
+    
 }
 "あいうえおかきくけこ…"
